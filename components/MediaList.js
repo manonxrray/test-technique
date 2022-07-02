c10bffe8e5c423a0f1a021274f1ac6daa5329276
Manon <manon.gerray@gmail.com>
2022-07-02 15:36:13 +0200

    MediaListCollection query & mapping

--- components/MediaList.js
+++ components/MediaList.js
@@ -1,8 +1,35 @@
+import { useState, useEffect } from "react";
 import Media from "./Media";
+import { useQuery } from "@apollo/client";
+import { MediaListQuery, MediaQuery } from "../graphql/query/media";
 
 const MediaList = () => {
+  const [medias, setMedias] = useState([]);
+  let mediaData = [];
+
+  const { loading, data } = useQuery(MediaListQuery, {
+    variables: { userId : 5916149 }
+  });
+  
+  useEffect(() => {
+    let entries = data.MediaListCollection.lists[0].entries;
+
+    for (let i =0; i < entries.length; i++) {
+      mediaData.push(entries[i].mediaId)
+    };
+
+    setMedias(mediaData);
+
+    console.log("🚀 ~ file: MediaList.js ~ line 8 ~ MediaList ~ medias", medias)
+    
+  }, []);
+
   return (
-    <Media />
+    <div>
+      {Object.values(medias).map((media) => {
+        return <Media />
+      })}
+    </div>
   )
 };
 
--- graphql/query/media.js
+++ graphql/query/media.js
@@ -1,27 +1,31 @@
 import { gql } from "@apollo/client";
 
-export const MediaListCollection = gql`
-  query MediaListCollection($userId: 5916149, type: ANIME){
-    lists {
-      entries {
-        mediaId
+export const MediaListQuery = gql`
+  query MediaListCollection ($userId: Int){
+    MediaListCollection(userId : $userId, type: ANIME) {
+      lists {
+        entries {
+          mediaId
+        }
       }
     }
   }
 `;
 
-export const Media = gql `
-  query Media($id : Int){
-    title {
-      romaji
-      english
-      native
-    }
-    coverImage {
-      extraLarge
-      large
-      medium
-      color
+export const MediaQuery = gql `
+  query Media ($id : Int){
+    media(id: $id) {
+      title {
+        romaji
+        english
+        native
+      }
+      coverImage {
+        extraLarge
+        large
+        medium
+        color
+      }
     }
   }
 `;
